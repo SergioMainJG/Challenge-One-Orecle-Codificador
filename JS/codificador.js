7056fb896734e5ba48036ddfd7cb6a50bfc99d09
SergioMainJG <al2223034493@azc.uam.mx>
2023-05-03 18:10:04 -0600

    Terminado?
    ya todo el JS amigo asjdajs

--- JS/codificador.js
+++ JS/codificador.js
@@ -1,20 +1,65 @@
-'use strict'
-
-function botonCodificar() {
-  let boton = document.getElementById('encriptar');
-  boton.addEventListener('click', codificarTexto());
-  boton.addEventListener('click', borrarImg());
-
-}
-
+'use strict';
+/**
+ * 
+ * 
+  La letra "a" es convertida para "ai"
+  la letra "e" es convertida para "enter"
+  La letra "i" es convertida para "imes"
+  La letra "o" es convertida para "ober"
+  La letra "u" es convertida para "ufat"
+ *
+ * 
+ **/
 function codificarTexto() {
-  const inputTextarea = document.getElementById('encriptar-text');
-  const outputTextarea = document.getElementById('encriptado-text');
+  let texto = document.getElementById('encriptar-text');
+  let validación = texto.value;
+  let patron = /[^a-z\s]/;
 
-  outputTextarea.value = 'Adios, Mundo';
-  inputTextarea.value = 'Hola, Mundo';
-}
+  if (patron.test(validación)) {
 
-function borrarImg() {
-  alert("Che página pendeja");
+    alert('El texto ingresado es inválido, solo letras minusculas y espacios');
+    let texto1 = document.getElementById('encriptar-text');
+    let texto2 = document.getElementById('encriptado-text');
+
+    texto1.value = '';
+    texto2.value = '';
+  } else {
+    const inputTextarea = document.getElementById('encriptar-text');
+
+    let textoModificado = () => {
+
+      let encriptando = inputTextarea.value;
+      let textoModificado = '';
+
+      for (let i = 0; i < encriptando.length; i++) {
+        switch (encriptando[i]) {
+          case 'a':
+            textoModificado += 'ai';
+            break;
+          case 'e':
+            textoModificado += 'enter';
+            break; 4
+          case 'i':
+            textoModificado += 'imes';
+            break;
+          case 'o':
+            textoModificado += 'ober';
+            break;
+          case 'u':
+            textoModificado += 'ufat';
+            break;
+          case ' ':
+            textoModificado += ' ';
+            break;
+          default:
+            textoModificado += encriptando[i];
+            break;
+        }
+      }
+      return textoModificado;
+    };
+    let outputTextarea = document.getElementById('encriptado-text');
+    outputTextarea.value = textoModificado(inputTextarea.value);
+    // document.getElementById('encriptado-text').innerHTML = textoModificado(inputTextarea.value);
+  };
 }
